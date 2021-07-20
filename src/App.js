241f3b9480d9617f45ee9f2df9faaa7bd3bcb5b2
petch <tayanchonbc@gmail.com>
2021-07-20 23:03:58 +0700

    edit json

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // import {Container,Label, Card, CardHeader, CardTitle, CardBody, Form, FormGroup, Row, Col, Progress } from "reactstrap"
-import { Container, Row, Col, Label, Button } from 'reactstrap';
-import { readFile } from "@ramonak/react-excel"
-import './custom.css'
-import DataTableComponent from "./components/datatableCustom.jsx"
-import { toast } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { Container, Row, Col, Label, Button, Table } from "reactstrap";
+import { readFile } from "@ramonak/react-excel";
+import "./custom.css";
+import DataTableComponent from "./components/datatableCustom.jsx";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-toast.configure()
+toast.configure();
 
 const ImportExcelDatatables = () => {
-
   //------------- fnc พิเศษจาก lib @ramonak/react-excel สำหรับดึง readfiles มาเอาลง datatables
-  function _interopDefault(ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-  var XLSX = _interopDefault(require('xlsx'));
+  function _interopDefault(ex) {
+    return ex && typeof ex === "object" && "default" in ex ? ex["default"] : ex;
+  }
+  var XLSX = _interopDefault(require("xlsx"));
 
   //---------------------------------constructor and State ---------------------------
-  const [textinputFile, setTextinputFile] = useState("")
-  const [initialValidData, setInitialValidData] = useState([])
-  const [initialData, setInitialData] = useState([])
-  const [rows, setRows] = useState([])
-  const [rows2, setRows2] = useState([])
-
+  const [textinputFile, setTextinputFile] = useState("");
+  const [initialData, setInitialData] = useState([]);
+  const [rows, setRows] = useState([]);
+  const [rows2, setRows2] = useState([]);
+  const [masterData, setMasterData] = useState([]);
   // -------------------- columns for datatables -------------------------------------
   const columns = [
     {
@@ -33,7 +33,14 @@ const ImportExcelDatatables = () => {
       ),
       theadAlign: "center",
       tbody: (row) => (
-        <div className="text-center" style={row.valid === false ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" } : { width: "100%" }}>
+        <div
+          className="text-center"
+          style={
+            row.valid === false
+              ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" }
+              : { width: "100%" }
+          }
+        >
           <Label>{row.empid}</Label>
         </div>
       ),
@@ -43,12 +50,21 @@ const ImportExcelDatatables = () => {
       name: "name",
       label: (
         <div>
-          <Label className="mb-1" need="need">name</Label>
+          <Label className="mb-1" need="need">
+            name
+          </Label>
         </div>
       ),
       theadAlign: "center",
       tbody: (row) => (
-        <div className="text-center" style={row.valid === false ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" } : { width: "100%" }}>
+        <div
+          className="text-center"
+          style={
+            row.valid === false
+              ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" }
+              : { width: "100%" }
+          }
+        >
           <Label>{row.name}</Label>
         </div>
       ),
@@ -58,12 +74,21 @@ const ImportExcelDatatables = () => {
       name: "lastname",
       label: (
         <div>
-          <Label className="mb-1" need="need">lastname</Label>
+          <Label className="mb-1" need="need">
+            lastname
+          </Label>
         </div>
       ),
       theadAlign: "center",
       tbody: (row) => (
-        <div className="text-center" style={row.valid === false ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" } : { width: "100%" }}>
+        <div
+          className="text-center"
+          style={
+            row.valid === false
+              ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" }
+              : { width: "100%" }
+          }
+        >
           <Label>{row.lastname}</Label>
         </div>
       ),
@@ -73,12 +98,21 @@ const ImportExcelDatatables = () => {
       name: "position",
       label: (
         <div>
-          <Label className="mb-1" need="need">position</Label>
+          <Label className="mb-1" need="need">
+            position
+          </Label>
         </div>
       ),
       theadAlign: "center",
       tbody: (row) => (
-        <div className="text-center" style={row.valid === false ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" } : { width: "100%" }}>
+        <div
+          className="text-center"
+          style={
+            row.valid === false
+              ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" }
+              : { width: "100%" }
+          }
+        >
           <Label>{row.position}</Label>
         </div>
       ),
@@ -88,21 +122,38 @@ const ImportExcelDatatables = () => {
       name: "startDates",
       label: (
         <div>
-          <Label className="mb-1" need="need">startDates</Label>
+          <Label className="mb-1" need="need">
+            startDates
+          </Label>
         </div>
       ),
       theadAlign: "center",
       tbody: (row) => (
-        <div className="text-center" style={row.valid === false ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" } : { width: "100%" }}>
+        <div
+          className="text-center"
+          style={
+            row.valid === false
+              ? { width: "100%", background: "rgba(220, 53, 69, 0.3)" }
+              : { width: "100%" }
+          }
+        >
           <Label>{row.startDates}</Label>
         </div>
       ),
       width: "20%",
     },
-  ]
+  ];
   //----------------------------useEffect ----------------------------------------------
   useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/petchbc22/react-excel-import/main/api.json"
+    )
+      .then((Response) => Response.json())
+      .then((data) => setMasterData(data))
+      .catch((err) => console.log(err));
+  }, []);
 
+  useEffect(() => {
     // if (initialData.SheetNames) {
     //   console.log()
     // }
@@ -112,75 +163,124 @@ const ImportExcelDatatables = () => {
           var _ref;
           var ws = initialData.Sheets[name];
           var dataParse = XLSX.utils.sheet_to_json(ws, {
-            header: [
-              'empid',
-              'name',
-              'lastname',
-              'position',
-              'startDates'
-            ],
+            header: ["empid", "name", "lastname", "position", "startDates"],
             defval: "",
           });
-          return _ref = {}, _ref = dataParse, _ref;
+          return (_ref = {}), (_ref = dataParse), _ref;
         });
-        let header = ''
-        result[0].slice(0, 1).map((item) =>
-          header += `${item.empid}${item.name}${item.lastname}${item.position}${item.startDates}`
-        )
-        if (header === 'empidnamelastnamepositionstartDates') {
-          // check header of xlsx is true 
+        let header = "";
+        result[0]
+          .slice(0, 1)
+          .map(
+            (item) =>
+              (header += `${item.empid}${item.name}${item.lastname}${item.position}${item.startDates}`)
+          );
+        if (header === "empidnamelastnamepositionstartDates") {
+          // check header of xlsx is true
           console.log("result[0].slice(2)", result[0].slice(2));
-          setRows(result[0].slice(2))
+          setRows(result[0].slice(2));
+        } else {
+          console.log("ข้อมูลผิด");
+          setTextinputFile("");
+          toast.error("header is not corresponding.", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 3000,
+          });
+          document.getElementById("inputFilesStudent").value = "";
         }
-        else {
-          console.log('ข้อมูลผิด')
-          setTextinputFile("")
-          toast.error('header is not corresponding.', { position: toast.POSITION.BOTTOM_RIGHT, autoClose: 3000 })
-          document.getElementById("inputFilesStudent").value = ""
-        }
-
-      }
+      };
       initialData && setData();
     }
-  }, [initialData])
+  }, [initialData]);
 
   //---------------------------------- FNC ALL ---------------------------------------
   const handleUpload = (e) => {
     // setStatusProcess(true)
-    setRows([])
+    setRows([]);
     // setRows2([])
-    const file = e.target.files[0]
+    const file = e.target.files[0];
     if (file) {
-      console.log('files---', file)
-      setTextinputFile(file.name)
+      console.log("files---", file);
+      setTextinputFile(file.name);
       readFile(file)
         .then((readedData) => setInitialData(readedData))
         .catch((error) => {
-          toast.error('file import is not xlsx format.',
-            {
-              position: toast.POSITION.BOTTOM_RIGHT,
-              autoClose: 3000
-            }
-          )
-        })
+          toast.error("file import is not xlsx format.", {
+            position: toast.POSITION.BOTTOM_RIGHT,
+            autoClose: 3000,
+          });
+        });
     }
+  };
+  const fncClear = () =>{
+    setRows([])
+    setTextinputFile("")
+    setInitialData([])
+    document.getElementById("inputFiles").value = ""
   }
-
-
 
   return (
     <>
       {/* components */}
       <Container>
         <Row className="py-1 justify-content-md-center pt-3">
-          <Col md="2" className="m-auto">
+          <Col md="12">
+            <div className="text-center">
+              <Label>Master Data</Label>
+            </div>
+            <Table>
+              <thead>
+                <tr>
+                  <th>emdID</th>
+                  <th>Name</th>
+                  <th>LastName</th>
+                  <th>Position</th>
+                  <th>StartDate</th>
+                </tr>
+              </thead>
+              <tbody>
+                {masterData ? (
+                  masterData.map((data, key) => {
+                    return (
+                      <tr key={key}>
+                        <th scope="row">{data.emdid}</th>
+                        <td>{data.name}</td>
+                        <td>{data.lastname}</td>
+                        <td>{data.position}</td>
+                        <td>{data.startDates}</td>
+                      </tr>
+                    );
+                  })
+                ) : (
+                  <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                  </tr>
+                )}
+              </tbody>
+            </Table>
+          </Col>
+          <Col md="2" className="m-auto pt-5">
             <Label>Excel import</Label>
           </Col>
-          <Col md="8">
+          <Col md="8" className="pt-5">
             <div style={{ width: "300px" }}>
               <div className=" custom-file">
-                <input className=" custom-file-input" id="inputFilesStudent" lang="en" type="file" accept=".xlsx" onChange={handleUpload}></input>
-                <label className=" custom-file-label m-0" htmlFor="inputFilesStudent">
+                <input
+                  className=" custom-file-input"
+                  id="inputFiles"
+                  lang="en"
+                  type="file"
+                  accept=".xlsx"
+                  onChange={handleUpload}
+                ></input>
+                <label
+                  className=" custom-file-label m-0"
+                  htmlFor="inputFilesStudent"
+                >
                   {textinputFile ? textinputFile : "select .XLSX "}
                 </label>
               </div>
@@ -190,21 +290,31 @@ const ImportExcelDatatables = () => {
         <Row>
           {rows.length ? (
             <>
-              <div className="col-12">
-                <DataTableComponent columns={columns} rows={rows} search={false} btnAdd={[]} />
+              <div className="col-12 text-center pt-5">
+                <Label>Your Data in XLSX</Label>
               </div>
-              <div className="col-12 text-center">
-                <Button color="success">import</Button>
+              <div className="col-12">
+                <DataTableComponent
+                  columns={columns}
+                  rows={rows}
+                  search={false}
+                  btnAdd={[]}
+                />
+              </div>
+              <div className="col-12 text-center row justify-content-center ">
+                <div className="m-1">
+                  <Button color="success">Validate</Button>
+                </div>
+                <div className="m-1">
+                  <Button color="danger" onClick={fncClear}>Clear</Button>
+                </div>
               </div>
             </>
           ) : null}
         </Row>
-
       </Container>
     </>
-  )
-}
-
-
+  );
+};
 
 export default ImportExcelDatatables;
